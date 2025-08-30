ea8ac01e7d8ded3c2d0eda1a7ddb789c2ddfb0e7
Madson <madson.mms@gmail.com>
2025-08-30 13:40:34 -0300

    Added bat sprite!

--- Enemies/Bat/bat.gd
+++ Enemies/Bat/bat.gd
@@ -1,7 +1,7 @@
 class_name Enemy
 extends CharacterBody2D
 
-@onready var sprite : Sprite2D = $Sprite2D
+@onready var sprite : Sprite2D = $Idle
 @onready var health_component : HealthComponent = $HealthComponent
 @onready var animation_handler : AnimationHandler = $AnimationHandler
 @onready var state_machine : StateMachine = $StateMachine
@@ -18,9 +18,9 @@ func _physics_process(_delta: float) -> void:
 		last_direction = direction
 		
 	if direction == Vector2.RIGHT:
-		sprite.scale.x = 1
-	else:
 		sprite.scale.x = -1
+	else:
+		sprite.scale.x = 1
 	
 	if health_component.health <= 0: 
 		queue_free()
---
+++ Enemies/Chicken/chicken.gd
@@ -0,0 +1,26 @@
+class_name Chicken
+extends CharacterBody2D
+
+@onready var sprite : Sprite2D = $Idle
+@onready var health_component : HealthComponent = $HealthComponent
+@onready var animation_handler : AnimationHandler = $AnimationHandler
+@onready var state_machine : StateMachine = $StateMachine
+@export var hurt_box: HurtBoxComponent
+
+var attacking : bool = false
+var direction : Vector2 = Vector2.DOWN
+var last_direction : Vector2 = Vector2.DOWN
+
+@export var move_speed : float = 20
+
+func _physics_process(_delta: float) -> void:
+	if direction != Vector2.ZERO:
+		last_direction = direction
+		
+	if direction == Vector2.RIGHT:
+		sprite.scale.x = -1
+	else:
+		sprite.scale.x = 1
+	
+	if health_component.health <= 0: 
+		queue_free()
